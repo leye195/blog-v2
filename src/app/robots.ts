dfb5201fc6a171ed37197447d2eb41c46f943b06
leye195 <leye195@naver.com>
2024-01-11 21:47:21 +0900

    feat: change robots.txt -> robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://www.dantechblog.xyz/sitemap.xml",
+  };
+}
