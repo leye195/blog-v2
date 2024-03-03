9c79fdbad35eb9d43b50eeb966217cec1fa43855
leye195 <leye195@naver.com>
2024-03-03 20:04:14 +0900

    feat: replace domains option to remotePatterns

--- next.config.js
+++ next.config.js
@@ -5,10 +5,19 @@ const nextConfig = {
   },
   reactStrictMode: true,
   images: {
-    domains: [
-      "www.notion.so",
-      "images.unsplash.com",
-      "s3.us-west-2.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.notion.so",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.us-west-2.amazonaws.com",
+      },
     ],
   },
 };
