9dcb1a81b39c38769e4ca8a484bb54a68b5cb849
leye195 <leye195@naver.com>
2024-08-24 16:44:33 +0900

    feat: add useEventListener hook

---
+++ src/hooks/useEventListener.ts
@@ -0,0 +1,20 @@
+import { DependencyList } from "react";
+import useIsomorphicLayoutEffect from "./useIsomorphicLayoutEffect";
+
+const useEventListener = (
+  event: keyof WindowEventMap | keyof DocumentEventMap,
+  callback: (e?: any) => void,
+  deps?: DependencyList
+) => {
+  useIsomorphicLayoutEffect(() => {
+    window.addEventListener(event, callback);
+
+    return () => {
+      window.removeEventListener(event, callback);
+    };
+  }, deps ?? []);
+
+  return null;
+};
+
+export default useEventListener;
