8a5dbd3a3e02d42b821ad0327226496557901f0e
leye195 <leye195@naver.com>
2025-06-29 00:36:41 +0900

    style: footer - change text color

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import { Email, GitHub, RSS } from '@/components/icon';
 
 const Footer = () => {
   return (
-    <div className="border-black-50 flex w-[inherit] flex-col items-center justify-center gap-3 border-t-2 bg-gray-200 py-8">
+    <div className="border-black-50 flex w-[inherit] flex-col items-center justify-center gap-3 border-t-2 bg-gray-200 py-8 text-white">
       <div className="flex gap-[10px] text-[24px] lg:text-[32px]">
         <a href="https://github.com/leye195" target="_blank" rel="noopener">
           <GitHub />
